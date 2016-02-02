2ee6d69b641b25cba53f7e079a74383ed3d08e84
Alex <me@codejitsu.net>
2016-02-02 08:03:29 +0100

    Added StdinProducer so I can pipe the output from wikiticker into it.

---
+++ krampus-producer/src/main/scala/krampus/producer/stdin/StdinProducer.scala
@@ -0,0 +1,19 @@
+// Copyright (C) 2015, codejitsu.
+
+package krampus.producer.stdin
+
+import akka.stream.scaladsl.Source
+import krampus.producer.WikiProducer
+
+object StdinProducer extends WikiProducer {
+  def main(args: Array[String]): Unit = {
+    sys.exit(run(args))
+  }
+
+  override def source(args: Array[String]): Source[String, Unit] = {
+    import scala.io.{Source => ioSource}
+
+    val source = ioSource.stdin
+    Source.fromIterator(() => source.getLines())
+  }
+}
