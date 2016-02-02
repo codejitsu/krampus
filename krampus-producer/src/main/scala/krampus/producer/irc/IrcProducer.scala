3198f27075f4c8e8a79422e4c509c84cbbad3447
Alex <me@codejitsu.net>
2016-02-02 07:17:02 +0100

    Refactoring: attempt to avoid InterruptedException in NIO with akka actors.

--- krampus-producer/src/main/scala/krampus/producer/irc/IrcProducer.scala
+++ krampus-producer/src/main/scala/krampus/producer/irc/IrcProducer.scala
@@ -2,20 +2,48 @@
 
 package krampus.producer.irc
 
-import akka.actor.{Actor, Props}
+import java.util.concurrent.TimeUnit
+
+import akka.actor.{Actor, ActorRef, Props}
 import akka.stream.actor.ActorPublisher
 import akka.stream.actor.ActorPublisherMessage.{Cancel, Request}
 import akka.stream.scaladsl.Source
-import com.metamx.common.lifecycle.Lifecycle
-import com.metamx.common.scala.lifecycle._
+import akka.util.Timeout
 import com.metamx.common.scala.Jackson
 import com.typesafe.scalalogging.LazyLogging
-import io.imply.wikiticker.ConsoleTicker._
 import io.imply.wikiticker.{IrcTicker, Message, MessageListener}
 import krampus.producer.WikiProducer
-import krampus.producer.irc.IrcPublisher.Publish
+import krampus.producer.irc.IrcActorPublisher.Publish
+import krampus.producer.irc.IrcReaderActor.StartReading
 
-class IrcPublisher() extends ActorPublisher[String] with LazyLogging {
+import scala.concurrent.Await
+import scala.util.Try
+
+class IrcReaderActor(pub: ActorRef, wikis: Array[String]) extends Actor {
+  lazy val listener = new MessageListener {
+    override def process(message: Message) = {
+      pub ! Publish(Jackson.generate(message.toMap))
+    }
+  }
+
+  lazy val ticker = new IrcTicker(
+    "irc.wikimedia.org",
+    "imply",
+    wikis map (x => s"#$x.wikipedia"),
+    Seq(listener)
+  )
+
+  override def receive: Actor.Receive = {
+    case StartReading => ticker.start()
+    case _ =>
+  }
+}
+
+object IrcReaderActor {
+  case object StartReading
+}
+
+class IrcActorPublisher() extends ActorPublisher[String] with LazyLogging {
   import scala.collection._
 
   val queue: mutable.Queue[String] = mutable.Queue()
@@ -44,7 +72,7 @@ class IrcPublisher() extends ActorPublisher[String] with LazyLogging {
   }
 }
 
-object IrcPublisher {
+object IrcActorPublisher {
   case class Publish(data: String)
 }
 
@@ -52,42 +80,24 @@ object IrcPublisher {
   * IRC based wikipedia entry producer.
   */
 object IrcProducer extends WikiProducer {
-  val dataPublisherRef = system.actorOf(Props[IrcPublisher])
+  val dataPublisherRef = system.actorOf(Props[IrcActorPublisher])
   val dataPublisher = ActorPublisher[String](dataPublisherRef)
 
+  var ircReader: ActorRef = _
+
   def main(args: Array[String]): Unit = {
+    import akka.pattern.ask
+    import scala.concurrent.duration._
+
+    implicit val timeout = Timeout(1, TimeUnit.MINUTES)
+
     val wikis = args.head.split(",")
 
-    val listener = new MessageListener {
-      override def process(message: Message) = {
-        dataPublisherRef ! Publish(Jackson.generate(message.toMap))
-      }
-    }
+    ircReader = system.actorOf(Props(new IrcReaderActor(dataPublisherRef, wikis)))
 
-    val ticker = new IrcTicker(
-      "irc.wikimedia.org",
-      "imply",
-      wikis map (x => s"#$x.wikipedia"),
-      Seq(listener)
-    )
+    val fut = ircReader ? StartReading
 
-    val lifecycle = new Lifecycle
-
-    lifecycle onStart {
-      ticker.start()
-    } onStop {
-      ticker.stop()
-    }
-
-    try {
-      lifecycle.start()
-    } catch {
-      case e: Throwable =>
-        log.error(e, "Failed to start up, stopping and exiting.")
-        lifecycle.stop()
-    }
-
-    Thread.sleep(30000)
+    Try(Await.ready(fut, 30 seconds))
 
     sys.exit(run(args))
   }
