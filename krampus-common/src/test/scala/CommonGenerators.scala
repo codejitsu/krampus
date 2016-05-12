4668378a30d36fa882fca3793c2853c1588df6fb
Alex <me@codejitsu.net>
2016-05-12 09:02:55 +0200

    Refactoring. Added new generator for RawKafkaMessage.

---
+++ krampus-common/src/test/scala/CommonGenerators.scala
@@ -0,0 +1,47 @@
+// Copyright (C) 2016, codejitsu.
+
+package krampus.entity
+
+import java.io.ByteArrayOutputStream
+
+import krampus.avro.WikiChangeEntryAvro
+import krampus.queue.RawKafkaMessage
+import org.apache.avro.io.EncoderFactory
+import org.apache.avro.specific.SpecificDatumWriter
+import org.scalacheck.Gen
+
+object CommonGenerators {
+  val wikiChangeEntryAvroGenerator: Gen[WikiChangeEntryAvro] = for {
+    isRobot <- Gen.oneOf(true, false)
+    channel <- Gen.alphaStr
+    timestamp <- Gen.posNum[Int]
+    flags <- Gen.alphaStr
+    isUnpatrolled <- Gen.oneOf(true, false)
+    page <- Gen.alphaStr
+    diffUrl <- Gen.identifier
+    added <- Gen.posNum[Int]
+    deleted <- Gen.posNum[Int]
+    comment <- Gen.alphaStr
+    isNew <- Gen.oneOf(true, false)
+    isMinor <- Gen.oneOf(true, false)
+    delta <- Gen.posNum[Int]
+    user <- Gen.alphaStr
+    namespace <- Gen.alphaStr
+  } yield new WikiChangeEntryAvro(isRobot, channel, timestamp.toString, flags, isUnpatrolled, page,
+    s"http://$diffUrl.com/diff", added, deleted, comment, isNew, isMinor, delta, user, namespace)
+
+  val rawKafkaMessageGenerator: Gen[RawKafkaMessage] = for {
+    entity <- wikiChangeEntryAvroGenerator
+  } yield {
+    val out = new ByteArrayOutputStream()
+    val encoder = EncoderFactory.get().binaryEncoder(out, null) // scalastyle:ignore
+    val writer = new SpecificDatumWriter[WikiChangeEntryAvro](WikiChangeEntryAvro.getClassSchema())
+
+    writer.write(entity, encoder)
+    encoder.flush()
+    out.close()
+    val serializedAvro = out.toByteArray()
+
+    RawKafkaMessage(entity.getChannel.toString.toCharArray.map(_.toByte), serializedAvro)
+  }
+}
--- krampus-common/src/test/scala/WikiChangeEntrySpecification.scala
+++ krampus-common/src/test/scala/WikiChangeEntrySpecification.scala
@@ -2,123 +2,101 @@
 
 package krampus.entity
 
-import krampus.avro.WikiChangeEntryAvro
 import org.joda.time.DateTime
-import org.scalacheck.{Gen, Prop, Properties}
+import org.scalacheck.{Prop, Properties}
 
 class WikiChangeEntrySpecification extends Properties("WikiChangeEntry") {
-  import Generators._
+  import CommonGenerators._
 
   import Prop.forAll
 
-  property("constructor: added") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: added") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.added == avroEntry.getAdded
   }
 
-  property("constructor: channel") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: channel") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.channel == avroEntry.getChannel.toString
   }
 
-  property("constructor: comment") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: comment") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.comment == avroEntry.getComment.toString
   }
 
-  property("constructor: deleted") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: deleted") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.deleted == avroEntry.getDeleted
   }
 
-  property("constructor: delta") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: delta") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.delta == avroEntry.getDelta
   }
 
-  property("constructor: diffUrl") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: diffUrl") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.diffUrl.toString == avroEntry.getDiffUrl
   }
 
-  property("constructor: flags") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: flags") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.flags == avroEntry.getFlags.toString.split(",").toList
   }
 
-  property("constructor: isMinor") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: isMinor") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.isMinor == avroEntry.getIsMinor
   }
 
-  property("constructor: isNew") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: isNew") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.isNew == avroEntry.getIsNew
   }
 
-  property("constructor: isRobot") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: isRobot") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.isRobot == avroEntry.getIsRobot
   }
 
-  property("constructor: isUnpatrolled") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: isUnpatrolled") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.isUnpatrolled == avroEntry.getIsUnpatrolled
   }
 
-  property("constructor: namespace") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: namespace") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.namespace == avroEntry.getNamespace.toString
   }
 
-  property("constructor: page") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: page") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.page == avroEntry.getPage.toString
   }
 
-  property("constructor: timestamp") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: timestamp") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.timestamp == new DateTime(avroEntry.getTimestamp.toString)
   }
 
-  property("constructor: user") = forAll(avroWikiChangeEntryGenerator) { avroEntry =>
+  property("constructor: user") = forAll(wikiChangeEntryAvroGenerator) { avroEntry =>
     val converted = WikiChangeEntry(avroEntry)
 
     converted.user == avroEntry.getUser.toString
   }
-}
-
-object Generators {
-  val avroWikiChangeEntryGenerator: Gen[WikiChangeEntryAvro] = for {
-    isRobot <- Gen.oneOf(true, false)
-    channel <- Gen.alphaStr
-    timestamp <- Gen.posNum[Int]
-    flags <- Gen.alphaStr
-    isUnpatrolled <- Gen.oneOf(true, false)
-    page <- Gen.alphaStr
-    diffUrl <- Gen.identifier
-    added <- Gen.posNum[Int]
-    deleted <- Gen.posNum[Int]
-    comment <- Gen.alphaStr
-    isNew <- Gen.oneOf(true, false)
-    isMinor <- Gen.oneOf(true, false)
-    delta <- Gen.posNum[Int]
-    user <- Gen.alphaStr
-    namespace <- Gen.alphaStr
-  } yield new WikiChangeEntryAvro(isRobot, channel, timestamp.toString, flags, isUnpatrolled, page,
-      s"http://$diffUrl.com/diff", added, deleted, comment, isNew, isMinor, delta, user, namespace)
 }
