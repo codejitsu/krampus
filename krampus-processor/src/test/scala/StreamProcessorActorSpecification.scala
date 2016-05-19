d14a899b112d0f4264f8ea8b071470b7b83a2be1
Alex <me@codejitsu.net>
2016-05-19 09:23:35 +0200

    Added more meat to the stream processor test.

--- krampus-processor/src/test/scala/StreamProcessorActorSpecification.scala
+++ krampus-processor/src/test/scala/StreamProcessorActorSpecification.scala
@@ -2,12 +2,22 @@
 
 package krampus.processor.actor
 
+import java.util
+
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
+import krampus.entity.WikiChangeEntry
+import krampus.processor.util.AppConfig
+import krampus.queue.RawKafkaMessage
 import net.manub.embeddedkafka.EmbeddedKafka
 import org.scalatest.concurrent.Eventually
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{BeforeAndAfterAll, FunSuiteLike, Matchers}
+import krampus.entity.CommonGenerators._
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.Serializer
+
+import scala.collection.mutable.ListBuffer
 
 class StreamProcessorActorSpecification() extends TestKit(ActorSystem("StreamProcessorActorSpecification"))
   with ImplicitSender
@@ -18,7 +28,50 @@ class StreamProcessorActorSpecification() extends TestKit(ActorSystem("StreamPro
   with EmbeddedKafka
   with Eventually {
 
-  test("StreamProcessorActor onMessage function receives deserialized kafka messages") {
+  implicit val serializer = new Serializer[RawKafkaMessage] {
+    private var isKey: Boolean = false
 
+    override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {
+      this.isKey = isKey
+    }
+
+    override def serialize(topic: String, data: RawKafkaMessage): Array[Byte] = if (isKey) {
+      data.key
+    } else {
+      data.msg
+    }
+
+    override def close(): Unit = ()
   }
+  
+  lazy val kafkaProducer = aKafkaProducer[RawKafkaMessage]
+  override def afterAll: Unit = TestKit.shutdownActorSystem(system)
+
+  test("StreamProcessorActor onMessage function receives deserialized kafka messages") {
+    withRunningKafka {
+      val messages = ListBuffer.empty[WikiChangeEntry]
+
+      def process(msg: WikiChangeEntry): Unit = messages += msg
+
+      val cnf = config
+      val actor = system.actorOf(StreamProcessorActor.props(cnf, process))
+      actor ! InitializeQueueListener
+
+      var counter = 0
+      forAll(rawKafkaMessageGenerator) { case (rawMessage, _) =>
+        kafkaProducer.send(new ProducerRecord(cnf.topic, rawMessage))
+        counter = counter + 1
+      }
+
+      Thread.sleep(5000) // scalastyle:ignore
+
+      eventually {
+        assert(messages.size == counter)
+      }
+
+      system.stop(actor)
+    }
+  }
+
+  def config: AppConfig = new AppConfig("cassandra-processor-app")
 }
