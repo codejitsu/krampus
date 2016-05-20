493d66ee67e650a116c855fb0f93b75fbf64b287
Alex <me@codejitsu.net>
2016-05-20 08:46:58 +0200

    Replaced LazyLogging with ActorLogging in AvroConverterActor.

--- krampus-processor/src/main/scala/krampus/processor/actor/AvroConverterActor.scala
+++ krampus-processor/src/main/scala/krampus/processor/actor/AvroConverterActor.scala
@@ -2,18 +2,17 @@
 
 package krampus.processor.actor
 
-import akka.actor.{Actor, ActorRef, Props}
-import com.typesafe.scalalogging.LazyLogging
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import krampus.avro.WikiChangeEntryAvro
 import krampus.entity.WikiChangeEntry
+import krampus.queue.RawKafkaMessage
 import org.apache.avro.io.DecoderFactory
 import org.apache.avro.specific.SpecificDatumReader
-import krampus.queue.RawKafkaMessage
 
 /**
   * Bytes to Avro converter actor.
   */
-class AvroConverterActor(recipient: ActorRef) extends Actor with LazyLogging { //TODO switch to ActorLogging
+class AvroConverterActor(recipient: ActorRef) extends Actor with ActorLogging {
   private[this] lazy val reader =
     new SpecificDatumReader[WikiChangeEntryAvro](WikiChangeEntryAvro.getClassSchema())
 
@@ -29,7 +28,7 @@ class AvroConverterActor(recipient: ActorRef) extends Actor with LazyLogging { /
     val decoder = DecoderFactory.get().binaryDecoder(msg.msg, null) //scalastyle:ignore
     val wikiChangeEntryAvro = reader.read(null, decoder) //scalastyle:ignore
 
-    logger.debug(s"${wikiChangeEntryAvro.getChannel()}: ${wikiChangeEntryAvro.getPage()}")
+    log.debug(s"${wikiChangeEntryAvro.getChannel()}: ${wikiChangeEntryAvro.getPage()}")
 
     wikiChangeEntryAvro
   }
