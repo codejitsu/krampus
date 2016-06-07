0fd7bb7a399b895f7782161114872305b1b6345f
Alex <me@codejitsu.net>
2016-06-07 08:20:39 +0200

    Added entities for Page and User.

---
+++ krampus-common/src/main/scala/krampus/entity/WikiPage.scala
@@ -0,0 +1,8 @@
+// Copyright (C) 2016, codejitsu.
+
+package krampus.entity
+
+import java.net.URL
+import java.util.UUID
+
+final case class WikiPage(id: UUID, title: String, url: URL)
---
+++ krampus-common/src/main/scala/krampus/entity/WikiUser.scala
@@ -0,0 +1,7 @@
+// Copyright (C) 2016, codejitsu.
+
+package krampus.entity
+
+import java.util.UUID
+
+final case class WikiUser(id: UUID, name: String, isRobot: Boolean)
