0954440cf5d59990982192f9f2ec641bd736f1df
Alex <me@codejitsu.net>
2016-06-08 09:03:01 +0200

    Added cassandra table for WikiPage entities.

---
+++ krampus-processor/src/main/scala/krampus/processor/cassandra/WikiPageTable.scala
@@ -0,0 +1,34 @@
+// Copyright (C) 2016, codejitsu.
+
+package krampus.processor.cassandra
+
+import java.net.URL
+
+import scala.concurrent.Future
+import com.websudos.phantom.dsl._
+import krampus.entity.WikiPage
+
+class Pages extends CassandraTable[ConcretePages, WikiPage] {
+  object Id extends UUIDColumn(this) with PartitionKey[UUID]
+  object Title extends StringColumn(this)
+  object Url extends StringColumn(this)
+
+  def fromRow(row: Row): WikiPage =
+    WikiPage(
+      Id(row),
+      Title(row),
+      new URL(Url(row))
+    )
+}
+
+abstract class ConcretePages extends Pages with RootConnector {
+  def store(page: WikiPage): Future[ResultSet] =
+    insert.value(_.Id, page.id).value(_.Title, page.title)
+      .value(_.Url, page.url.toString)
+      .consistencyLevel_=(ConsistencyLevel.ALL)
+      .future()
+
+  def getById(id: UUID): Future[Option[WikiPage]] = {
+    select.where(_.Id eqs id).one()
+  }
+}
