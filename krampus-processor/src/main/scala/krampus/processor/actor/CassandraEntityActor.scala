92f83b4b1da1e994e400114574a9e60c22d2dab5
Alex <me@codejitsu.net>
2016-07-03 11:05:32 +0200

    Added an implicit parameter to the CassandraEntityActor in order to inject DAO implementation.

--- krampus-processor/src/main/scala/krampus/processor/actor/CassandraEntityActor.scala
+++ krampus-processor/src/main/scala/krampus/processor/actor/CassandraEntityActor.scala
@@ -3,10 +3,11 @@
 package krampus.processor.actor
 
 import akka.actor.{Actor, ActorLogging, Props}
+import krampus.processor.cassandra.CassandraDao
 
 import scala.reflect.ClassTag
 
-class CassandraEntityActor[E](implicit ev: ClassTag[E]) extends Actor with ActorLogging {
+class CassandraEntityActor[E](implicit ev: ClassTag[E], dao: CassandraDao[E]) extends Actor with ActorLogging {
   override def receive: Receive = {
     case Store(e) if e.getClass == ev.runtimeClass =>
       log.debug(s"Processing message $e of class ${ev.runtimeClass.getCanonicalName}")
@@ -15,5 +16,5 @@ class CassandraEntityActor[E](implicit ev: ClassTag[E]) extends Actor with Actor
 }
 
 object CassandraEntityActor {
-  def props[E](implicit ev: ClassTag[E]): Props = Props(new CassandraEntityActor[E])
+  def props[E](implicit ev: ClassTag[E], dao: CassandraDao[E]): Props = Props(new CassandraEntityActor[E])
 }
--- krampus-processor/src/test/scala/CassandraUserEntityActorSpecification.scala
+++ krampus-processor/src/test/scala/CassandraUserEntityActorSpecification.scala
@@ -5,13 +5,21 @@ package krampus.processor.actor
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
-import org.scalatest.{BeforeAndAfterAll, FunSuiteLike, Matchers}
+import org.scalatest.{BeforeAndAfterAll, Matchers}
 import krampus.entity.CommonGenerators._
 import krampus.entity.WikiUser
+import krampus.processor.cassandra.{EmbeddedCassandraDatabaseProvider, EmbeddedCassandraSuite}
 
-class CassandraUserEntityActorSpecification() extends TestKit(ActorSystem("CassandraUserEntityActorSpecification")) with ImplicitSender
-  with FunSuiteLike with Matchers with GeneratorDrivenPropertyChecks with BeforeAndAfterAll {
+class CassandraUserEntityActorSpecification() extends EmbeddedCassandraSuite
+  with Matchers with GeneratorDrivenPropertyChecks with BeforeAndAfterAll with EmbeddedCassandraDatabaseProvider {
+
+  val testKit = new TestKit(ActorSystem("CassandraUserEntityActorSpecification")) with ImplicitSender
+  
+  import testKit._
+
   override def afterAll: Unit = TestKit.shutdownActorSystem(system)
+
+  implicit val db = database.Users
 
   test("CassandraEntityActor for User entity must receive Store(user) messages") {
     val actor = system.actorOf(CassandraEntityActor.props[WikiUser])
