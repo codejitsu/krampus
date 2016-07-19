e6ea3a1bb0cd3a3a21bea4ee26629f65bad5ac4d
Alex <me@codejitsu.net>
2016-07-19 20:58:12 +0200

    Added reprocessing on failure.

--- krampus-processor/src/main/scala/krampus/processor/actor/CassandraEntityActor.scala
+++ krampus-processor/src/main/scala/krampus/processor/actor/CassandraEntityActor.scala
@@ -2,21 +2,35 @@
 
 package krampus.processor.actor
 
-import akka.actor.Status.Failure
 import akka.actor.{Actor, ActorLogging, Props}
-import akka.pattern.pipe
 import krampus.processor.cassandra.CassandraDao
 
 import scala.reflect.ClassTag
+import scala.util.{Failure, Success}
 
 class CassandraEntityActor[E](implicit ev: ClassTag[E], dao: CassandraDao[E]) extends Actor with ActorLogging {
   implicit val ec = context.dispatcher
 
+  var thresholdErrors = 10000 // scalastyle:ignore
+
   override def receive: Receive = {
-    case Store(e, back, caller) if e.getClass == ev.runtimeClass => {
+    case store @ Store(e, back, caller) if e.getClass == ev.runtimeClass => {
       log.debug(s"Processing message $e of class ${ev.runtimeClass.getCanonicalName}")
       val concreteEntity = e.asInstanceOf[E]
-      dao.store(concreteEntity) map { res => StoreResult(concreteEntity, back, caller) } pipeTo self
+      val storeF = dao.store(concreteEntity)
+
+      storeF onComplete {
+        case Success(r) => self ! StoreResult(concreteEntity, back, caller)
+        case Failure(th) => {
+          thresholdErrors = thresholdErrors - 1
+
+          if (thresholdErrors < 0) {
+            log.error("Too many cassandra errors.")
+          }
+
+          self ! store
+        }
+      }
     }
 
     case Store(_, back, _) => back ! InvalidEntityType
