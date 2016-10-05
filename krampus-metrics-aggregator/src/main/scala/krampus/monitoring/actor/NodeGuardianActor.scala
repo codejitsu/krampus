a57ddd309d6aa382039911c149637be21827c7c0
Alex <me@codejitsu.net>
2016-10-05 06:48:58 +0200

    The first step in order to remove the duplicate KafkaListenerActor from 'metrics-aggregator' module.

--- krampus-metrics-aggregator/src/main/scala/krampus/monitoring/actor/NodeGuardianActor.scala
+++ krampus-metrics-aggregator/src/main/scala/krampus/monitoring/actor/NodeGuardianActor.scala
@@ -2,22 +2,63 @@
 
 package krampus.monitoring.actor
 
-import akka.actor.{Props, Actor}
-import com.typesafe.scalalogging.LazyLogging
-import krampus.monitoring.util.AppConfig
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import krampus.actor.protocol.{InitializeQueueListener, MessageConverted, QueueListenerInitialized, StartStreamProcessor}
+import krampus.actor.{AvroConverterActor, KafkaListenerActor => NewKafkaListenerActor}
+import krampus.entity.WikiEdit
+import krampus.monitoring.util.{AggregationMessage, AppConfig}
+import krampus.queue.RawKafkaMessage
+import krampus.monitoring.util.AppConfig.ConfigDuration
+import scala.collection.mutable
 
 /**
   * Root actor node.
   */
-class NodeGuardianActor(appConfig: AppConfig) extends Actor with LazyLogging {
-  val kafkaListener = context.actorOf(KafkaListenerActor.props(appConfig), "kafka-listener")
+class NodeGuardianActor(config: AppConfig) extends Actor with ActorLogging {
+  private[this] lazy val statsdConnection =
+    (config.aggregationConfig.getString("statsd.host"), config.aggregationConfig.getInt("statsd.port"))
+
+  private[this] val statsdGateway =
+    new StatsD(context, statsdConnection._1, statsdConnection._2,
+      packetBufferSize = config.aggregationConfig.getInt("statsd.packet-buffer-size"))
+
+  private[this] lazy val allCounter =
+    context.actorOf(CounterActor.props[AggregationMessage]("all-messages",
+      config.aggregationConfig.getMillis("flush-interval-ms"),
+      _ => true, statsdGateway), "all-messages-counter")
+
+  private[this] lazy val counters: mutable.Map[String, ActorRef] = mutable.Map.empty
+
+  private[this] val avroConverter = context.actorOf(AvroConverterActor.props(self), "avro-converter")
+  //TODO rename after refactoring
+  private[this] val kafkaListener = context.actorOf(NewKafkaListenerActor.props(config.kafkaConfig, processKafkaMessage),
+    "kafka-listener")
+
+  private[this] def processKafkaMessage(msg: RawKafkaMessage): Unit = avroConverter ! msg
 
   override def receive: Receive = {
-    case StartListener =>
-      kafkaListener ! InitializeListener
+    case StartStreamProcessor =>
+      log.debug("Start initializing kafka listener.")
+      kafkaListener ! InitializeQueueListener
 
-    case ListenerInitialized =>
-      logger.info("Kafka listener initialized.")
+    case QueueListenerInitialized =>
+      log.info("Kafka listener initialized.")
+
+    case MessageConverted(msg) =>
+      log.debug(s"Message converted: $msg")
+      onMessage(msg)
+  }
+  
+  def onMessage(msg: WikiEdit): Unit = {
+    val aggMsg = AggregationMessage(msg)
+
+    val channelCounter = counters.getOrElseUpdate(msg.channel,
+      context.actorOf(CounterActor.props[AggregationMessage](msg.channel,
+        config.aggregationConfig.getMillis("flush-interval-ms"),
+        e => e.msg.channel == msg.channel, statsdGateway), s"${msg.channel.drop(1)}-counter"))
+
+    allCounter ! aggMsg
+    channelCounter ! aggMsg
   }
 }
 
