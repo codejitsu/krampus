d2d9594b46b24799fe6a1dcc4c9e3d50ec5fa9f1
Alex <me@codejitsu.net>
2016-10-25 07:12:12 +0200

    Added a better date time generator.

--- krampus-common/src/test/scala/CommonGenerators.scala
+++ krampus-common/src/test/scala/CommonGenerators.scala
@@ -8,14 +8,39 @@ import krampus.avro.WikiEditAvro
 import krampus.queue.RawKafkaMessage
 import org.apache.avro.io.EncoderFactory
 import org.apache.avro.specific.SpecificDatumWriter
+import org.joda.time.{DateTime, DateTimeZone, LocalDateTime}
 import org.scalacheck.Gen
 
 object CommonGenerators {
+  val noOfDays = 23
+
+  val now = DateTime.now()
+  val dates = for {
+    t0 <- 0 until noOfDays
+  } yield {
+    now.minusDays(t0)
+  }
+
+  def sameDayTimestampGen(date: DateTime): Gen[DateTime] = for {
+    hourOfDay <- Gen.choose(0, 23) // scalastyle:ignore
+    minuteOfHour <- Gen.choose(0, 59) // scalastyle:ignore
+  } yield {
+    val dtz = DateTimeZone.forID("Europe/Berlin")
+    val ldt = new LocalDateTime(date.getMillis, dtz).withTime(hourOfDay, minuteOfHour, 0, 0)
+    ldt.toDateTime(DateTimeZone.UTC)
+  }
+
+  val timestampGen: Gen[DateTime] = for {
+    date: DateTime <- Gen.oneOf(dates)
+    timestamp <- sameDayTimestampGen(date)
+  } yield timestamp
+
+
   val wikiEditAvroGenerator: Gen[WikiEditAvro] = for {
     uuid <- Gen.uuid
     isRobot <- Gen.oneOf(true, false)
     channel <- Gen.alphaStr
-    timestamp <- Gen.posNum[Int]
+    timestamp <- timestampGen
     flags <- Gen.alphaStr
     isUnpatrolled <- Gen.oneOf(true, false)
     page <- Gen.alphaStr
