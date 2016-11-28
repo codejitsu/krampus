824b9eaa1fd4cd065ff0e01ec02ccf247e5f673a
Alex <me@codejitsu.net>
2016-11-28 06:36:19 +0100

    Added preloaded grafana dashboard. Metrics will be loaded automatically.

--- compose-resources/grafana/init/grafana-init.sh
+++ compose-resources/grafana/init/grafana-init.sh
@@ -3,7 +3,7 @@
 response=$(curl -sL -w "%{http_code}\\n" "http://admin:admin@statsd:80/api/datasources" -o /dev/null)
 
 while [ $response != 200 ]; do
-    echo "Grafana is not up...${response}"
+    echo "Grafana is not up yet... Response code: ${response}"
     sleep 1
     response=$(curl -sL -w "%{http_code}\\n" "http://admin:admin@statsd:80/api/datasources" -o /dev/null)
 done
